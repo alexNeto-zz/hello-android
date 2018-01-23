9b186f8540e0ddc6d10bff0cf409cd868d041775
alexNeto <alex.neto3@hotmail.com>
2018-01-22 23:06:11 -0200

    iniciado novo projeto

---
+++ agenda-java/app/src/main/java/com/alexneto/agenda_java/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alexneto.agenda_java
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
