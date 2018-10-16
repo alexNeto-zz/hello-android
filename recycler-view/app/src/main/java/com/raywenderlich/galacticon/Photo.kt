b184d9d8c441789d19ddc4fe278ec67c18063206
alexNeto <alex.neto3@hotmail.com>
2018-10-16 14:12:34 -0300

    adicionado ripple effect

--- recycler-view/app/src/main/java/com/raywenderlich/galacticon/Photo.kt
+++ recycler-view/app/src/main/java/com/raywenderlich/galacticon/Photo.kt
@@ -31,45 +31,45 @@ import java.util.*
 
 class Photo(photoJSON: JSONObject) : Serializable {
 
-  private lateinit var photoDate: String
-  lateinit var humanDate: String
-    private set
-  lateinit var explanation: String
-    private set
-  lateinit var url: String
-    private set
+    private lateinit var photoDate: String
+    lateinit var humanDate: String
+        private set
+    lateinit var explanation: String
+        private set
+    lateinit var url: String
+        private set
 
-  init {
-    try {
-      photoDate = photoJSON.getString(PHOTO_DATE)
-      humanDate = convertDateToHumanDate()
-      explanation = photoJSON.getString(PHOTO_EXPLANATION)
-      url = photoJSON.getString(PHOTO_URL)
-    } catch (e: JSONException) {
-      e.printStackTrace()
+    init {
+        try {
+            photoDate = photoJSON.getString(PHOTO_DATE)
+            humanDate = convertDateToHumanDate()
+            explanation = photoJSON.getString(PHOTO_EXPLANATION)
+            url = photoJSON.getString(PHOTO_URL)
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+
     }
 
-  }
+    private fun convertDateToHumanDate(): String {
 
-  private fun convertDateToHumanDate(): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+        val humanDateFormat = SimpleDateFormat("dd MMMM yyyy")
+        try {
+            val parsedDateFormat = dateFormat.parse(photoDate)
+            val cal = Calendar.getInstance()
+            cal.time = parsedDateFormat
+            return humanDateFormat.format(cal.time)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+            return ""
+        }
 
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd")
-    val humanDateFormat = SimpleDateFormat("dd MMMM yyyy")
-    try {
-      val parsedDateFormat = dateFormat.parse(photoDate)
-      val cal = Calendar.getInstance()
-      cal.time = parsedDateFormat
-      return humanDateFormat.format(cal.time)
-    } catch (e: ParseException) {
-      e.printStackTrace()
-      return ""
     }
 
-  }
-
-  companion object {
-    private val PHOTO_DATE = "date"
-    private val PHOTO_EXPLANATION = "explanation"
-    private val PHOTO_URL = "url"
-  }
+    companion object {
+        private val PHOTO_DATE = "date"
+        private val PHOTO_EXPLANATION = "explanation"
+        private val PHOTO_URL = "url"
+    }
 }
